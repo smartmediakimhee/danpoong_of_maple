a9832ebadaac80115d62d5a043d9e138f727453f
aa <aa@DESKTOP-OCIGRN5>
2017-12-03 16:57:56 +0900

    준영 : 리뷰쓰기 베타

--- danpoong/src/com/service/ReviewInsertService.java
+++ danpoong/src/com/service/ReviewInsertService.java
@@ -12,10 +12,13 @@ public class ReviewInsertService extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
+		String drink_name = request.getParameter("drink_name");
 		String review_content = request.getParameter("review_content");
 		String score = request.getParameter("score");
 		
 		
+		System.out.println(id);
+		System.out.println(drink_name);
 		System.out.println(review_content);
 		System.out.println(score);
 	}
